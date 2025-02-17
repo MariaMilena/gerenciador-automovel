29e8879a35517c8f4943b566fafd40b6e2e59093
Maria Milena <milena20182018@gmail.com>
2025-02-16 23:37:21 -0300

    fix: edit

--- src/main/java/com/xwz/frota/api/domain/tipoCombustivel/TipoCombustivel.java
+++ src/main/java/com/xwz/frota/api/domain/tipoCombustivel/TipoCombustivel.java
@@ -1,8 +1,17 @@
 package com.xwz.frota.api.domain.tipoCombustivel;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum TipoCombustivel {
-    GASOLINA,
-    ETANOL,
-    DIESEL,
-    FLEX
+    ETANOL, DIESEL, GASOLINA, FLEX;
+
+    @JsonCreator
+    public static TipoCombustivel fromString(String value) {
+        return TipoCombustivel.valueOf(value.toUpperCase()); // Converte para maiúsculas
+    }
+
+    @JsonValue
+    public String toJson() {
+        return this.name();
+    }
 }
